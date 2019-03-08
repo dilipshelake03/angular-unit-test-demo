fd9bdc8df1bc899cc5244404451dcdafe02517a9
Dilip Shelake <d.shelake@easternenterprise.com>
2019-03-08 18:50:20 +0530

    unit test changes

---
+++ src/app/auth.service.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  beforeEach(() => {
+    /*TestBed.configureTestingModule({
+      providers: [AuthService]
+    }); */
+    service = new AuthService();
+  });
+
+  afterEach(() => {
+    service = null;
+    localStorage.removeItem('token');
+  });
+
+  it('should return true from isAuthenticated when there is a token', () => {
+    localStorage.setItem('token', '1234');
+    expect(service.isAuthenticated()).toBeTruthy();
+  });
+
+  it('should return false from isAuthenticated when there is no token', () => {
+    expect(service.isAuthenticated()).toBeFalsy();
+  });
+});

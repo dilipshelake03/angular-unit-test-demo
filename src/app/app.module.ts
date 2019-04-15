e54164878926cf7196767077c2d5007a8530743d
Dilip Shelake <d.shelake@easternenterprise.com>
2019-04-15 18:02:11 +0530

    testing routes with ATB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RouterAtbComponent } from './router-atb/router-atb.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RouterAtbComponent
   ],
   imports: [
     BrowserModule
